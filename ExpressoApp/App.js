7ad49574db8f1ff629ffcab6ee0efcf2987b0195
PScandle <vzp7444@autuni.ac.nz>
2021-08-26 09:21:05 +1200

    fixed merge conflict

--- ExpressoApp/App.js
+++ ExpressoApp/App.js
@@ -6,8 +6,7 @@
  * @flow strict-local
  */
 
-import React, { Component } from "react";
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
     SafeAreaView,
     ScrollView,
@@ -30,62 +29,22 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import firebase from 'firebase';
 
-import AddMenuItemScreen from './AddMenuItemScreen';
+import AddMenuItemScreen from './screens/AddMenuItemScreen';
+import RegisterUserScreen from './screens/RegisterUserScreen';
 
 const Stack = createStackNavigator();
 
-class App extends Component{
-    constructor() {
-        super();
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        let firebaseConfig = {
-            apiKey: "AIzaSyAn92Ew0Z5VJ_TgThlS_krQHUUBW8zzuOE",
-            authDomain: "expresso-418d1.firebaseapp.com",
-            databaseURL: "https://expresso-418d1-default-rtdb.firebaseio.com",
-            projectId: "expresso-418d1",
-            storageBucket: "expresso-418d1.appspot.com",
-            messagingSenderId: "723640216847",
-            appId: "1:723640216847:web:65558223bfa0ac1ac2a27a",
-            measurementId: "G-SR7PKGX02H"
-        };
-        // Initialize Firebase
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig);
-        }else {
-            firebase.app(); // if already initialized, use that one
-        }
-
-        const dbRef = firebase.database().ref();
-        dbRef.child("Users/Owner").child("Owner1").get().then((snapshot) => {
-            if (snapshot.exists()) {
-                console.log(snapshot.val());
-            } else {
-                console.log("No data available");
-            }
-        }).catch((error) => {
-            console.error(error);
-        });
-
-        firebase.database().ref("Users/Owner/Owner2").set({
-            firstName: 'Shayla',
-            lastName: 'Craigs',
-            phone: '024756437',
-        }).then(() => {
-            console.log("owner inserted");
-        })
-    }
-
+class App extends Component {
     render() {
         return (
             <NavigationContainer>
-                <Stack.Navigator screenOptions={{headerShown: false,}}>
+                <Stack.Navigator screenOptions={{headerShown: false}}>
+                    <Stack.Screen name="RegisterUser" component={RegisterUserScreen}/>
                     <Stack.Screen name="AddMenuItem" component={AddMenuItemScreen} />
                 </Stack.Navigator>
             </NavigationContainer>
         );
     }
-
 };
 
 const styles = StyleSheet.create({
