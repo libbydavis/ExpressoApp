7769137bab00841973076b33a56e9a29c84e039f
amnw12 <48080226+amnw12@users.noreply.github.com>
2021-09-14 22:25:28 +1200

    updated grid items to have rounded edges

--- ExpressoApp/screens/search/SearchScreen.js
+++ ExpressoApp/screens/search/SearchScreen.js
@@ -199,6 +199,7 @@ const styles = StyleSheet.create({
     margin: 10,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius: 10,
   },
   dropDownPicker: {
     fontFamily: 'Monserrat-Regular',
@@ -225,6 +226,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '100%',
     height: 160,
+    borderRadius: 10,
   },
   itemText: {
     justifyContent: 'center',
