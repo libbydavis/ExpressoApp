c1d834a9eef7aae15287f2c0fa5d489eeb891db5
PScandle <vzp7444@autuni.ac.nz>
2021-09-15 18:19:48 +1200

    added menuview page

---
+++ ExpressoApp/screens/menu/MenuView.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import {
+    StyleSheet,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text,
+    Image, ToastAndroid,
+} from 'react-native';
+import { firebase, firebaseDB } from "../../firebase/FirebaseConfig";
+
+export const MenuView = ({ navigation }) => {
+
+}
