93db58bba3ff27833b8f5a958e13a9f3df483314
PScandle <vzp7444@autuni.ac.nz>
2021-09-15 19:42:47 +1200

    setup framework for menu view

--- ExpressoApp/screens/menu/MenuView.js
+++ ExpressoApp/screens/menu/MenuView.js
@@ -10,7 +10,7 @@ import {
 } from 'react-native';
 import {firebase, firebaseDB} from "../../firebase/FirebaseConfig";
 
-export const MenuView = ({navigation}) => {
+export const MenuView = ({ navigation }) => {
     const [returnedMenuID, setReturnedMenuID] = useState('');
     const [menuList, setMenuList] = useState('');
     const user = firebase.auth().currentUser;
@@ -24,8 +24,8 @@ export const MenuView = ({navigation}) => {
             .get().then((snapshot) => {
             if (snapshot.exists()) {
                 if (!menuList) {
-//                    setMenuList(); TODO init list of menus, iterate through possibilities
-                } else {        // TODO and add to list
+//                    setMenuList(); TODO initialise list of menus, iterate through
+                } else {          // TODO possibilities and add to list
                     console.log('No data available')
                 }
             }
@@ -33,12 +33,139 @@ export const MenuView = ({navigation}) => {
             console.error(error);
         })
 
-        //iterate through list of menus
-        let currentID = '';
-        let menuItems = [];
-        const menus = [];
-        const iterateMenus = (obj) => {
+        // TODO iterate through list of menus to display relevant ones
+//        let currentID = '';
+//        let menuItems = [];
+//        const menus = [];
+//        const iterateMenus = (obj) => {
 
         }
     })
+
+    return (
+        <></>
+    )
 }
+
+export default MenuView;
+
+const styles = StyleSheet.create({
+    mainView: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    mainTitle: {
+        fontFamily: 'Monserrat-Regular',
+        color: '#25a2af',
+        fontSize: 35,
+    },
+    scrollView: {
+        marginHorizontal: 20,
+        // backgroundColor: '#ffffff',
+        marginBottom: 30,
+        paddingBottom: 100,
+        alignItems: 'center',
+    },
+    orders: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    navBar: {
+        marginBottom: 15,
+        marginTop: 8,
+        alignSelf: 'flex-start',
+    },
+    headerIcon: {
+        width: 200,
+        height: 50,
+    },
+    modalView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+    },
+    textInput: {
+        fontFamily: 'Monserrat-Regular',
+        borderBottomWidth: 1,
+        borderStartWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+        paddingRight: 50,
+        marginBottom: 20,
+    },
+    title: {
+        fontFamily: 'Monserrat-Bold',
+        color: '#25a2af',
+        fontSize: 35,
+        margin: 10,
+    },
+    rowView: {
+        flexDirection: 'row',
+        marginTop: 20,
+    },
+    columnView: {
+        flexDirection: 'column',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    expressoButton: {
+        backgroundColor: '#25a2af',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10,
+    },
+    discardButton: {
+        backgroundColor: 'red',
+        borderRadius: 10,
+        padding: 10,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    expressoButtonText: {
+        color: '#ffffff',
+    },
+    imagePicker: {
+        backgroundColor: 'red',
+    },
+    inputChecklist: {
+        flexDirection: 'row',
+        marginTop: 10,
+    },
+    enterOptionText: {
+        borderBottomWidth: 1,
+        borderStartWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+        paddingRight: 100,
+        marginRight: 15,
+    },
+    enterOptionTitle: {
+        borderBottomWidth: 1,
+        borderStartWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+        position: 'absolute',
+        top: 15,
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    quantityElements: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    optionBottomButtons: {
+        flexDirection: 'row',
+        marginTop: 10,
+    },
+    expressoLabel: {
+        fontFamily: 'Monserrat-Regular',
+        color: '#383838',
+    },
+});
+
