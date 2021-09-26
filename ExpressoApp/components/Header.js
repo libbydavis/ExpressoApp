5f9e2dbd49d7963741c107b07a09700e0b0366e0
cf-whyte <cfwhytedev@gmail.com>
2021-09-27 11:03:02 +1300

    Abstracted 'ExpressoHeader' component

--- ExpressoApp/components/Header.js
+++ ExpressoApp/components/Header.js
@@ -1,2 +1,84 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, TouchableOpacity, Image, StyleSheet} from 'react-native';
+
+class ExpressoHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        const {navigation, rightOption} = this.props;
+        this.navigation = navigation;
+        this.rightOption = rightOption; //Cart or Profile
+        this.styles = StyleSheet.create({
+            expressoLogoButton: {
+                marginBottom: 15,
+                marginTop: 8,
+                alignSelf: 'flex-start',
+            },
+            expressoLogo: {
+                width: 200,
+                height: 50,
+            },
+            cart: {
+                width: 40,
+                height: 40,
+                marginTop: 8,
+                marginRight: 10,
+            },
+            profile: {
+                width: 40,
+                height: 40,
+                marginTop: 8,
+                marginRight: 10,
+            },
+            rightOption: {
+                width: 40,
+                height: 40,
+                marginTop: 8,
+                marginRight: 10,
+            },
+        });
+    }
+
+    OnLogoPressHandler = () => {
+        try {
+            this.navigation.navigate('SearchScreen')?.();
+        } catch (error) {
+            console.warn('Caught an error in ExpressoButton');
+            console.error(error.message);
+        }
+    }
+
+    OnRightOptionPressHandler = () => {
+        try {
+            this.rightOption === 'cart' ?
+                this.navigation.navigate('CartScreen') :
+                this.navigation.navigate('ProfileScreen');
+        } catch (error) {
+            console.warn('Caught an error in ExpressoButton');
+            console.error(error.message);
+        }
+    }
+
+
+    render() {
+        return (
+            <View>
+                <TouchableOpacity style={this.styles.expressoLogoButton} onPress={this.OnLogoPressHandler}>
+                    <Image source={require('../../assets/ExpressoLogo.png')} style={this.styles.expressoLogo}/>
+                </TouchableOpacity>
+                { /*Options like cart or profile */
+                    this.rightOption === 'cart' ?
+                        <TouchableOpacity style={this.styles.rightOption} onPress={this.OnPressHandler}>
+                            <Image source={require('../../assets/carticon.png')} style={this.styles.cart}/>
+                        </TouchableOpacity> :
+                        this.rightOption === 'profile' ?
+                            <TouchableOpacity style={this.styles.rightOption} onPress={this.OnPressHandler}>
+                                <Image source={require('../../assets/profileIcon.png')} style={this.styles.profile}/>
+                            </TouchableOpacity> :
+                            null
+                }
+            </View>
+        );
+    }
+}
+
+export default ExpressoHeader;
