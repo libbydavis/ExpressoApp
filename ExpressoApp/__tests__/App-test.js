b237f1eb67f3f51082dabb31ef2ce84d18db5eac
libbydavis <libstarnz@gmail.com>
2021-09-28 15:40:39 +1300

    working on testing

--- ExpressoApp/__tests__/App-test.js
+++ ExpressoApp/__tests__/App-test.js
@@ -7,8 +7,8 @@ import React from 'react';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('renders', () => {
+  render(<App />);
 });
