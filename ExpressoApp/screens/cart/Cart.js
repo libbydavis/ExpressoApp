28befe184a0e32371c8f26c1b06a56caffdc5ee5
libbydavis <libstarnz@gmail.com>
2021-09-28 22:49:35 +1300

    added more options when adding item to cart

--- ExpressoApp/screens/cart/Cart.js
+++ ExpressoApp/screens/cart/Cart.js
@@ -130,12 +130,12 @@ class Cart extends Component {
           <View>
             {
               this.state.items.map((item, index) => {
-                return <CartItem key={index} image={item.image} title={item.title} price={item.price} quantity={item.quantity} receiveQuantity={(value) => this.setNewQuantity(value, index)} onpress={() => this.deleteCartItem(index, item.totalPrice)}></CartItem>
+                return <CartItem key={index} cartItem={{image: item.image, title: item.title, price: item.price, quantity: item.quantity, notes: item.notes, options: item.options}} receiveQuantity={(value) => this.setNewQuantity(value, index)} onpress={() => this.deleteCartItem(index, item.totalPrice)}></CartItem>
               })
             }
           </View>
           <View>
-            <Text>Total: ${this.state.total}</Text>
+            <Text>Total: ${this.state.total.toFixed(2)}</Text>
           </View>
         </View>
 
--- ExpressoApp/screens/createmenu/ReviewMenuItemScreen.js
+++ ExpressoApp/screens/createmenu/ReviewMenuItemScreen.js
@@ -1,21 +1,44 @@
 import React, {Component, useState} from "react";
-import {Text, View, StyleSheet, Image, TouchableOpacity, TouchableWithoutFeedback} from "react-native";
+import {Text, View, StyleSheet, Image, TouchableOpacity, TouchableWithoutFeedback, TextInput} from "react-native";
 import AddToCartButton from "../../components/AddToCartButton";
 import QuantityInput from "../../components/QuantityInput";
 import Header from "../../components/Header";
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
 
 //TODO: options lists
-//TODO: make nav bar into it's own component
 
 const ReviewMenuItemScreen = ({navigation, route}) => {
-    const [quantity, setQuantity] = useState(1);
-    const UNIT_PRICE = route.params.item.price;
-    const [currentPrice, setCurrentPrice] = useState(route.params.item.price);
+    const UNIT_PRICE = route.params.price;
+    const [cartItem, setCartItem] = useState({
+        title: route.params.title,
+        price: UNIT_PRICE,
+        currentPrice: route.params.price,
+        quantity: 1,
+        notes: '',
+        options: ''
+    })
 
     const receiveQuantity = (value) => {
-        setQuantity(quantity);
+        setCartItem({...this.state, ['quantity']: value});
         let newPrice = UNIT_PRICE * value;
-        setCurrentPrice(newPrice);
+        setCartItem({...cartItem, ['currentPrice']: newPrice});
+    }
+
+    const setNotes = (notes) => {
+        setCartItem({...cartItem, ['notes']: notes});
+    }
+
+    const toggleOptions = (item, title, isChecked) => {
+        let optionsCopy = [...cartItem.options];
+        if (isChecked) {
+            optionsCopy = [...optionsCopy, item];
+            setCartItem({...cartItem, ['options']: optionsCopy});
+        }
+        else {
+            let index = cartItem.options.indexOf(item);
+            optionsCopy.splice(index, 1);
+            setCartItem({...cartItem, ['options']: optionsCopy});
+        }
     }
 
     return (
@@ -26,13 +49,36 @@ const ReviewMenuItemScreen = ({navigation, route}) => {
             </TouchableWithoutFeedback>
             <View style={styles.pageView}>
                 <View style={styles.contentView}>
-                    <Image source={{uri: route.params.item.image}}></Image>
-                    <Text style={styles.title}>{route.params.item.title}</Text>
-                    <Text style={styles.description}>{route.params.item.description}</Text>
+                    <Image source={{uri: route.params.image}}></Image>
+                    <Text style={styles.title}>{cartItem.title}</Text>
+                    <Text style={styles.description}>{route.params.description}</Text>
+                    <QuantityInput receiveValue={receiveQuantity} initialQuantity={cartItem.quantity}></QuantityInput>
 
-                    <QuantityInput receiveValue={receiveQuantity} initialQuantity={quantity}></QuantityInput>
-                    <Text style={styles.price}>$ {currentPrice}</Text>
-                    <AddToCartButton item={{title: route.params.item.title, price: UNIT_PRICE, quantity: quantity}}></AddToCartButton>
+                    <View style={styles.checkListView}>
+                        {
+                            route.params.optionLists.map((item, index) => {
+                                return (
+                                    <View key={index} style={styles.checklist}>
+                                        <Text style={styles.checkListTitle}>{item.title}</Text>
+                                        {
+                                            item.options.map((innerItem, innerIndex) => {
+                                                return <BouncyCheckbox iconStyle={{borderColor: '#25a2af'}}
+                                                                       fillColor={'#25a2af'}
+                                                                       text={innerItem}
+                                                                       style={styles.checkItem}
+                                                                       key={innerIndex}
+                                                                       onPress={(isChecked) => toggleOptions(innerItem, item.title, isChecked)}/>
+                                            })
+                                        }
+                                    </View>
+                                )
+
+                            })
+                        }
+                    </View>
+                    <Text style={styles.price}>$ {cartItem.currentPrice}</Text>
+                    <TextInput onChangeText={(text) => setNotes(text)} placeholder={"additional notes"} style={styles.notesBox} multiline={true}/>
+                    <AddToCartButton item={cartItem}></AddToCartButton>
                 </View>
             </View>
         </View>
@@ -66,6 +112,31 @@ const styles = StyleSheet.create({
         fontFamily: 'Monserrat-ExtraBold',
         marginLeft: 22,
         fontSize: 15
+    },
+    checkListView: {
+        marginBottom: 20,
+        flexDirection: 'row'
+    },
+    checkItem: {
+        marginBottom: 3,
+        marginTop: 3,
+    },
+    notesBox: {
+        borderBottomWidth: 1,
+        borderStartWidth: 1,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+        paddingLeft: 20,
+        paddingRight: 20,
+        marginBottom: 15
+    },
+    checkListTitle: {
+        marginBottom: 5,
+        fontSize: 16
+    },
+    checklist: {
+        marginRight: 25
     }
 
 })
