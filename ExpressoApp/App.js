49305e4479675ab3e479a43be3be90bc3b025b43
cf-whyte <cfwhytedev@gmail.com>
2021-10-10 14:50:12 +1300

    Pushing branch for more changes

--- ExpressoApp/App.js
+++ ExpressoApp/App.js
@@ -20,6 +20,7 @@ import SearchScreen from './screens/search/SearchScreen';
 import ReviewMenuItemScreen from "./screens/createmenu/ReviewMenuItemScreen";
 import CreateMenuScreen from "./screens/createmenu/CreateMenuScreen";
 import MenuScreen from "./screens/menu/MenuScreen";
+import DateTime from "./components/DateTimePicker";
 
 
 const Stack = createStackNavigator();
@@ -28,50 +29,52 @@ const Stack = createStackNavigator();
  *
  */
 export default class App extends Component {
-  /**
-   *
-   * @return {JSX.Element}
-   */
-  constructor() {
-    super();
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    SplashScreen.hide();
-    const firebaseConfig = {
-      apiKey: 'AIzaSyDFDrnM-_MnV2Zg-wgY3Vgn5J9LmwdMvZc',
-      authDomain: 'expresso-418d1.firebaseapp.com',
-      databaseURL: 'https://expresso-418d1-default-rtdb.firebaseio.com',
-      projectId: 'expresso-418d1',
-      storageBucket: 'expresso-418d1.appspot.com',
-      messagingSenderId: '723640216847',
-      appId: '1:723640216847:web:65558223bfa0ac1ac2a27a',
-      measurementId: 'G-SR7PKGX02H',
-    };
-      // Initialize Firebase
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-    } else {
-      firebase.app(); // if already initialized, use that one
+    /**
+     *
+     * @return {JSX.Element}
+     */
+    constructor() {
+        super();
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        SplashScreen.hide();
+        const firebaseConfig = {
+            apiKey: 'AIzaSyDFDrnM-_MnV2Zg-wgY3Vgn5J9LmwdMvZc',
+            authDomain: 'expresso-418d1.firebaseapp.com',
+            databaseURL: 'https://expresso-418d1-default-rtdb.firebaseio.com',
+            projectId: 'expresso-418d1',
+            storageBucket: 'expresso-418d1.appspot.com',
+            messagingSenderId: '723640216847',
+            appId: '1:723640216847:web:65558223bfa0ac1ac2a27a',
+            measurementId: 'G-SR7PKGX02H',
+        };
+        // Initialize Firebase
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        } else {
+            firebase.app(); // if already initialized, use that one
+        }
     }
-  }
 
-  render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="AddMenuItem" component={AddMenuItemScreen}/>
-          <Stack.Screen name="LoginScreen" component={LoginScreen}/>
-          <Stack.Screen name="CreateStorePageScreen" component={CreateStorePageScreen}/>
-          <Stack.Screen name="CreateMenuScreen" component={CreateMenuScreen} />
-          <Stack.Screen name="RegisterUser" component={RegisterUserScreen}/>
-          <Stack.Screen name="OwnerOrdersScreen" component={OrdersScreen}/>
-          <Stack.Screen name='SearchScreen' component={SearchScreen}/>
+    render() {
+        return (
+            <NavigationContainer>
+                <Stack.Navigator screenOptions={{headerShown: false}}>
 
-          <Stack.Screen name="ReviewMenuItem" component={ReviewMenuItemScreen} />
-          <Stack.Screen name="Cart" component={CartScreen} />
-          <Stack.Screen name="MenuScreen" component={MenuScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-  }
+                    <Stack.Screen name="LoginScreen" component={LoginScreen}/>
+                    <Stack.Screen name="Cart" component={CartScreen}/>
+                    <Stack.Screen name="AddMenuItem" component={AddMenuItemScreen}/>
+                    <Stack.Screen name="DateTime" component={DateTime}/>
+                    <Stack.Screen name="CreateStorePageScreen" component={CreateStorePageScreen}/>
+                    <Stack.Screen name="CreateMenuScreen" component={CreateMenuScreen}/>
+                    <Stack.Screen name="RegisterUser" component={RegisterUserScreen}/>
+                    <Stack.Screen name="OwnerOrdersScreen" component={OrdersScreen}/>
+                    <Stack.Screen name='SearchScreen' component={SearchScreen}/>
+
+                    <Stack.Screen name="ReviewMenuItem" component={ReviewMenuItemScreen}/>
+                    <Stack.Screen name="MenuScreen" component={MenuScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
