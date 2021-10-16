5c852c862b4fb95e9373656ba63efad729c953e6
libbydavis <libstarnz@gmail.com>
2021-10-16 23:53:47 +1300

    mocked async storage and doing testing for review menu item

---
+++ ExpressoApp/__tests__/ReviewMenuItemScreen-test.js
@@ -0,0 +1,17 @@
+// __tests__/ReviewMenuItemScreen-test.js
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ReviewMenuItemScreen from '../screens/createmenu/ReviewMenuItemScreen';
+import {shallow} from "react-native/jest/renderer";
+
+
+test('renders correctly', () => {
+    const reviewMenuItem = renderer.create(<ReviewMenuItemScreen route={{params : {title: 'hotdog', price: 10, description: 'tasty hotdog', optionLists: []}}}/>).toJSON();
+    expect(reviewMenuItem).toMatchSnapshot();
+});
+
+test('state changes correctly', () => {
+    const wrapper = shallow(<ReviewMenuItemScreen route={{params : {title: 'hotdog', price: 10, description: 'tasty hotdog', optionLists: []}}}></ReviewMenuItemScreen>)
+    expect(wrapper.state).toEqual({ title: 'hotdog', price: 10, currentPrice: 10, quantity: 1, notes: '', options: ''});
+
+});
